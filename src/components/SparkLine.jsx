6b71507e76b208632425a070bd7fd9accd6d1adc
Joramba <jorkabozko@gmail.com>
2023-05-23 18:28:17 +0200

    feat: add revenue section

---
+++ src/components/SparkLine.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SparkLineComponents } from "@syncfusion/ej2-react-charts";
+
+const SparkLine = () => {
+  return <div>SparkLine</div>;
+};
+
+export default SparkLine;
